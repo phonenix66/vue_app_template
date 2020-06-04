3ebbd4875553851d61abd911ff6e1fcc1f4bf7b9
phonenix66 <37186187@163.com>
2020-06-04 16:24:39 +0800

    vue

--- src/config/env.production.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    title: '管理系統',
-    baseUrl: 'http://www.test.com',
-    baseApi: 'https://www.xxx.com/api',
-    APPID: 'xxx',
-    APPSECRET: 'xxx'
-}
--- src/config/index.js
+++
@@ -1,3 +0,0 @@
-const config = require('./env.' + process.env.VUE_APP_ENV);
-
-module.exports = config;
